44f6184270b94fdcc95b21b2937cb21c6b2b11f7
Macbook <Macbook@Macbooks-MBP-2.lan>
2020-11-09 21:08:42 +0100

    Add new structure for the code

--- script.js
+++ script.js
@@ -1,42 +1,48 @@
-window.addEventListener('scroll', function(event) {
-  var scroll = this.scrollY;
+window.addEventListener('load', main);
 
-  console.log(window. pageYOffset);
-  
-  if(scroll > 100) {
-    let lineOne = document.getElementById('lineOne');
-    lineOne.style.height = '100%';
-  }
-  if(scroll > 1400) {
-    let lineTwo = document.getElementById('lineTwo');
-    lineTwo.style.height = '100%';
-  }
-  if(scroll > 2500) {
-    let lineThree = document.getElementById('lineThree');
-    lineThree.style.height = '100%';
-  }
-  if(scroll > 3600) {
-    let lineFour = document.getElementById('lineFour');
-    lineFour.style.height = '100%';
-  }
-  if(scroll > 4600) {
-    let lineFive = document.getElementById('lineFive');
-    lineFive.style.height = '100%';
-  }
-  if(scroll > 5800) {
-    let lineSix = document.getElementById('lineSix');
-    lineSix.style.height = '100%';
-  }
-  if(scroll > 7100) {
-    let lineSeven = document.getElementById('lineSeven');
-    lineSeven.style.height = '100%';
-  }
-  if(scroll > 8200) {
-    let lineEight = document.getElementById('lineEight');
-    lineEight.style.height = '100%';
-  }
-  if(scroll > 9300) {
-    let lineNine = document.getElementById('lineNine');
-    lineNine.style.height = '100%';
-  }
-});
+function main() {
+    getAnimatedLines();
+}
+
+function getAnimatedLines() {
+    window.addEventListener('scroll', function(event) {
+        let scrolled = this.scrollY;
+        
+        if(scrolled > 100) {
+          let lineOne = document.getElementById('lineOne');
+          lineOne.style.height = '100%';
+        }
+        if(scrolled > 1400) {
+          let lineTwo = document.getElementById('lineTwo');
+          lineTwo.style.height = '100%';
+        }
+        if(scrolled > 2500) {
+          let lineThree = document.getElementById('lineThree');
+          lineThree.style.height = '100%';
+        }
+        if(scrolled > 3600) {
+          let lineFour = document.getElementById('lineFour');
+          lineFour.style.height = '100%';
+        }
+        if(scrolled > 4600) {
+          let lineFive = document.getElementById('lineFive');
+          lineFive.style.height = '100%';
+        }
+        if(scrolled > 5800) {
+          let lineSix = document.getElementById('lineSix');
+          lineSix.style.height = '100%';
+        }
+        if(scrolled > 7100) {
+          let lineSeven = document.getElementById('lineSeven');
+          lineSeven.style.height = '100%';
+        }
+        if(scrolled > 8200) {
+          let lineEight = document.getElementById('lineEight');
+          lineEight.style.height = '100%';
+        }
+        if(scrolled > 9300) {
+          let lineNine = document.getElementById('lineNine');
+          lineNine.style.height = '100%';
+        }
+      });
+}
