43da8bbf93219414bf2fe429a32fb9c5f6e2c426
Macbook <Macbook@Macbooks-MBP-2.lan>
2020-11-13 11:05:51 +0100

    Add changes to getAnimatedLines

--- script.js
+++ script.js
@@ -6,46 +6,55 @@ function main() {
 }
 
 function getAnimatedLines() {
-    window.addEventListener('scroll', function(event) {
-        let scrolled = this.scrollY;
-        
-        if(scrolled > 100) {
-          let lineOne = document.getElementById('lineOne');
-          lineOne.style.height = '100%';
-        }
-        if(scrolled > 1400) {
-          let lineTwo = document.getElementById('lineTwo');
-          lineTwo.style.height = '100%';
-        }
-        if(scrolled > 2500) {
-          let lineThree = document.getElementById('lineThree');
-          lineThree.style.height = '100%';
-        }
-        if(scrolled > 3600) {
-          let lineFour = document.getElementById('lineFour');
-          lineFour.style.height = '100%';
-        }
-        if(scrolled > 4600) {
-          let lineFive = document.getElementById('lineFive');
-          lineFive.style.height = '100%';
-        }
-        if(scrolled > 5800) {
-          let lineSix = document.getElementById('lineSix');
-          lineSix.style.height = '100%';
-        }
-        if(scrolled > 7100) {
-          let lineSeven = document.getElementById('lineSeven');
-          lineSeven.style.height = '100%';
-        }
-        if(scrolled > 8200) {
-          let lineEight = document.getElementById('lineEight');
-          lineEight.style.height = '100%';
-        }
-        if(scrolled > 9300) {
-          let lineNine = document.getElementById('lineNine');
-          lineNine.style.height = '100%';
-        }
-      });
+  window.addEventListener('scroll', function(event) {
+      let scrolled = this.scrollY;
+      
+      let lineOne = document.getElementById('lineOne');
+      let lineTwo = document.getElementById('lineTwo');
+      let lineThree = document.getElementById('lineThree');
+      let lineFour = document.getElementById('lineFour');
+      let lineFive = document.getElementById('lineFive');
+      let lineSix = document.getElementById('lineSix');
+      let lineSeven = document.getElementById('lineSeven');
+      let lineEight = document.getElementById('lineEight');
+      let lineNine = document.getElementById('lineNine');
+      
+      if(scrolled > lineOne.offsetTop - window.innerHeight /2) {
+        lineOne.style.height = '100%';
+      }
+      
+      if(scrolled > lineTwo.offsetTop - window.innerHeight /2) {
+        lineTwo.style.height = '100%';
+      }
+      
+      if(scrolled > lineThree.offsetTop - window.innerHeight /2) {
+        lineThree.style.height = '100%';
+      }
+      
+      if(scrolled > lineFour.offsetTop - window.innerHeight /2) {
+        lineFour.style.height = '100%';
+      }
+      
+      if(scrolled > lineFive.offsetTop - window.innerHeight /2) {
+        lineFive.style.height = '100%';
+      }
+      
+      if(scrolled > lineSix.offsetTop - window.innerHeight /2) {
+        lineSix.style.height = '100%';
+      }
+      
+      if(scrolled > lineSeven.offsetTop - window.innerHeight /2) {
+        lineSeven.style.height = '100%';
+      }
+      
+      if(scrolled > lineEight.offsetTop - window.innerHeight /2) {
+        lineEight.style.height = '100%';
+      }
+      
+      if(scrolled > lineNine.offsetTop - window.innerHeight /2) {
+        lineNine.style.height = '100%';
+      }
+    });
 }
 
 function getTextFadeIn () {
